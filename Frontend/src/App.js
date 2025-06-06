32b4ffec6b6bbb034e893f5f859ecbc014cd118c
Shubham Malhotra <141633563+Shubhammalhotra001@users.noreply.github.com>
2025-06-07 02:17:52 +0530

    Updates in Frontend Committed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/dashboard';
 
-// Protected Route Component
+// Protected Route Componen
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
   return token ? children : <Navigate to="/" />;
